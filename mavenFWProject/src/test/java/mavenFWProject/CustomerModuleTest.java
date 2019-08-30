82acd3b4682583c027ec800d9f35111ec485f456
hp-pc <hp-pc@HP_Laptop>
2019-08-30 14:42:34 +0530

    Created by Nazneen

---
+++ mavenFWProject/src/test/java/mavenFWProject/CustomerModuleTest.java
@@ -0,0 +1,20 @@
+package mavenFWProject;
+
+import org.testng.annotations.Test;
+
+public class CustomerModuleTest {
+	
+	@Test
+	public void createCustomerTest()
+	{
+		System.out.println("customer created successfully");
+	}
+	
+	@Test
+	public void modifyCustomerTest()
+	{
+		System.out.println("customer modified successfully");
+	}
+	
+
+}
